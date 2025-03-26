b7a58aa331b75786906270760be1592b33af6793
Nika <nikushapanchulidze04@gmail.com>
2025-03-26 17:22:26 +0400

    first commit

--- src/routes/transaction-router.js
+++ src/routes/transaction-router.js
@@ -1,7 +1,6 @@
 const { authenticate } = require("../controllers/auth-controller");
 const { createTransaction, webhook, successfullyPaid, failedPaid } = require("../controllers/transaction-controller");
 const transactionSchema = require("../schemas/transaction-schema");
-const responseSchema = require("../schemas/response-schema");
 
 async function transactionRoutes(fastify, options) {
   // Using preHandler hook for handling authentication
@@ -12,7 +11,7 @@ async function transactionRoutes(fastify, options) {
     createTransaction
   );
 
-  fastify.post("/webhook", {schema: responseSchema}, webhook);
+  fastify.post("/webhook", webhook);
   fastify.get('/webhook/success', successfullyPaid);
   fastify.get('/webhook/fail', failedPaid);
 }
